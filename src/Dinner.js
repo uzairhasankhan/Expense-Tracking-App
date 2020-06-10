7db7c71f5ea3990582dda65365aa241544a032d6
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-10 20:57:46 +0500

    Created new component

---
+++ src/Dinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dinner() {
+
+    return ( <div>
+    <h1>We will eat Beef Biryani.</h1>
+    <h1>While Abu will eat Beaf Curry with Roti .</h1>
+    </div>
+    );
+    
+}
+
+export default Dinner;

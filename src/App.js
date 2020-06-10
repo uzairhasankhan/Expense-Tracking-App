934009b3cba40163b41eb17c5f56b519054a99e2
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-11 03:54:25 +0500

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React,{useState}  from 'react';
-
+import { Header } from './components/Header';
 import './App.css';
 
-
-
 function App() {
-    let [count, setCount] = useState(100);
-    let [isLite, setDay] = useState(false)
+    
     
     return (
     
-    <div className={`box ${isLite? "dayLight" : "nightLight"}`}>
-    {/* <Msg counter={count}/>    */}
-
-    <h1>Value of counters is {count}</h1>
-    
-    
-    <button className="Button" onClick={
-        ()=> setCount(++count) 
-    }> 
-     Update Counter</button>
-
-    <hr />
-    <br />
-
-    <h1>Day time: {isLite ? 'Morning' : 'Night'}</h1>
-    
-     <br />
-    
-    <button className="Button" onClick={()=> setDay(!isLite)}> Set Light</button>
+    <div>
+    <Header />
     </div>
     );
 }

86fad9f11aef4d694a2c44a6231ee444f2440257
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-11 04:44:57 +0500

    Update App.js, App.css and make Balance.js, Income.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
 import './App.css';
 
 function App() {
@@ -9,6 +11,10 @@ function App() {
     
     <div>
     <Header />
+    <div className="container">
+        <Balance />
+        <IncomeExpenses />
+    </div>
     </div>
     );
 }
---
+++ src/components/Balance.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+          <h3>Your Balance</h3>
+          <h1 id="balance"> PKR 0.00 </h1>  
+        </>
+    )
+}

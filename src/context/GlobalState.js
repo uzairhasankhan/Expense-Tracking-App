ae7d0cb12f0fa407bdd64a34581b38c2d26bcec3
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-12 03:51:36 +0500

    Create Global Component

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {createContext, useReducer} from 'react'
+import AppReducer from './AppReducer';
 
-export const GlobalState = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const initialState = {
+    transactions: [
+          { id: 1, text: 'Flower', amount: -20 },
+          { id: 2, text: 'Salary', amount: 300 },
+          { id: 3, text: 'Book', amount: -10 },
+          { id: 4, text: 'Camera', amount: 150 }
+        ]
 }
+// Craete Context
+export const GlobalContext =createContext(initialState);
+
+//Provider Component
+export const GlobalProvider =({ Children}) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {Children}
+        </GlobalContext.Provider>);
+}
+

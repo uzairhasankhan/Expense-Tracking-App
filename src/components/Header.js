f45b4a3f644171095a5149f7de3ad2ace483b048
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-12 20:01:59 +0500

    Header.js updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-           Expense Tracker App by UHK
+           Expense Tracker App by <a href="Uzair Hasan Khan"> <strong>UHK</strong></a>
         </h2>
     )
 }

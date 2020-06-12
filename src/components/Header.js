3f19150371e31e46e5e7e9ef980360590dd529e8
Uzair Hasan Khan <66217404+uzairhasankhan@users.noreply.github.com>
2020-06-12 20:34:02 +0500

    Logo Added in Title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <h2>
-           Expense Tracker App by <a href="Uzair Hasan Khan"> <strong>UHK</strong></a>
+           Expense Tracker App by <abbr title="Uzair Hasan Khan"> UHK</abbr>
         </h2>
     )
 }
